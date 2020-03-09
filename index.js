af3f7804675e079574052f637f9df3fc3b448a4a
Antonio Jose Jimenez Segovia <antoniojosejimenezsegovia@gmail.com>
2020-03-09 20:21:05 +0000

    origin

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+var app = express();
+
+app.use("/", express.static("./public"));
+
+app.get("/time", (request, response) => {
+	response.send("<html>"+new Date()+"</html>");
+});
+
+app.listen(80, () => {
+	console.log("Server ready.")
+});
+
+console.log("Loading Server...")
+console.log(new Date());
