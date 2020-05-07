5b54db34f0e01fcb3133ee8862a32c18be4fadfb
Jesus Vazquez <jesusvazquezrivadeneyra@gmail.com>
2020-05-07 18:39:22 +0200

    Cambios para el D02

--- index.js
+++ index.js
@@ -1,60 +1,21 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const dataStore = require("nedb");
-const path = require("path");
-const globalSuicidesAPI = require(path.join(__dirname,"global-suicides_API"));
-const marriagesAPI = require(path.join(__dirname,"marriagesAPI"));
-const globalDivorcesAPI = require(path.join(__dirname,"global-divorcesAPI"));
+const express = require("express");  //
+const bodyParser = require("body-parser"); //
+//const marriagesAPI = require("src/back/marriagesAPI");  
+const path = require ("path");
+const marriagesAPI =require(path.join(__dirname,"./src/back/marriagesAPI"));
 
+var app = express();//
 
-var port = process.env.PORT || 80;
-const dbGSuicides = path.join(__dirname,"global-suicides_API/suicides.db");
+app.use(bodyParser.json());   //
 
-const app = express();
-app.use(bodyParser.json());
+marriagesAPI(app);   
 
+var port = process.env.PORT || 9999; //
 
 app.use("/", express.static("./public"));
 
-
-
-
-
-const BASE_API_URL = "/api/v1"; //Esta línea debemos copiarla en cada carpeta
-/*====================================================*/
-/*========= Antonio Jose Jimenez Segovia =========*/
-/*====================================================*/
-
-const globalSuicidesDb = new dataStore({							//objeto
-	filename: dbGSuicides,
-	autoload: true
-});
-
-
-globalSuicidesAPI(app, globalSuicidesDb);
-
-/*====================================================*/
-/*========= API_Jesús Vázquez Rivadeneyra =========*/
-/*====================================================*/
-
-marriagesAPI(app);
-
-/*====================================================*/
-/*========= Juan Manuel Cortés Alonso =========*/
-/*====================================================*/
-
-globalDivorcesAPI(app);
-/*====================================================*/
-/*================== WEB_SERVER_TIME ================*/
-/*===================================================*/
-app.get("/time", (request, response) => {
-	response.send("<html>"+new Date()+"</html>");
-});
-
 app.listen(port, () => {
-	console.log("Server ready.");
+    console.log("Server ready on port " + port);
 });
 
-console.log("Loading Server...");
- console.log(new Date());
-console.log("Connected on port: " + port);
+console.log("Starting server...");
